a87300128f3ffae08f2ec4ed48df6beb8424710d
Claudia Lissette Gutiérrez Díaz <132630774+ClaudiaGtzD@users.noreply.github.com>
2024-07-25 00:52:16 -0600

    Proyecto Final

--- Caso de Prueba 04/ForecastsCaseofStudy.R
+++ Caso de Prueba 04/ForecastsCaseofStudy.R
@@ -17,7 +17,7 @@ library(stringr)
 #####################################################################
 #PASO 2. SE DEFINEN LAS RUTAS DE CATALOGOS Y RESULTADOS.
 
-ruta.1 <- "C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/"
+ruta.1 <- "C:/Users/riemm/OneDrive/Escritorio/Business Intelligence/"
 
 ################################################################################
 ################################################################################
@@ -31,7 +31,7 @@ ruta.1 <- "C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Ne
 #PASO 3. LEE INSUMOS PRONOSTICO IPC
 
 
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor1.csv",header=TRUE, sep=",")
+Competitor1 <- read.csv("C:/Users/riemm/OneDrive/Hazel and Big Data/Business Intelligence/Competidor1.csv",header=TRUE, sep=",")
 
 ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
 plot(ts.Competidor1)
@@ -47,217 +47,7 @@ Forecast.Competidor1
 plot(Forecast.Competidor1,main="Competidor1 Forecasting",
      ylab="Competidor1 Volume")
 
-ts.Competidor1 <- ts(Competitor1$Product2, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
 
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
 
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor2.csv",header=TRUE, sep=",")
 
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor3.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-ts.Competidor1 <- ts(Competitor1$Product2, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor4.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-  
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor5.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-ts.Competidor1 <- ts(Competitor1$Product2, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor6.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor7.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-ts.Competidor1 <- ts(Competitor1$Product2, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor8.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor9.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-ts.Competidor1 <- ts(Competitor1$Product2, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
-
-Competitor1 <- read.csv("C:/Users/Claudia/OneDrive/Documentos/FCFM/04_Tetramestre/Modelo de Negocios/BusinessIntelligence/Caso de Prueba 04/Competidor10.csv",header=TRUE, sep=",")
-
-ts.Competidor1 <- ts(Competitor1$Product1, frequency = 52, start = c(2019,1))
-plot(ts.Competidor1)
-
-ARIMA.Competidor1 <- auto.arima(ts.Competidor1,D=1)
-summary(ARIMA.Competidor1)
-plot(ARIMA.Competidor1$x,col="blue")
-par(new=TRUE)
-plot(ARIMA.Competidor1$fitted,col="red") 
-title(main="Real vs Forecast")
-Forecast.Competidor1<- forecast(ARIMA.Competidor1,25,level = 99)
-Forecast.Competidor1
-plot(Forecast.Competidor1,main="Competidor1 Forecasting",
-     ylab="Competidor1 Volume")
 
--- README.md
+++ README.md
@@ -10,6 +10,38 @@ Este repositorio está hecho para recopilar la información de la materia de _Mo
 - [Curso Power BI](./Curso%20Power%20BI/Curso%20Power%20BI/Reporte_RRHH.pbix)
 
 ### Semana 01: Social Network:
-- [Caso de estudio: Social Network](./Caso%20de%20Prueba%2001/Social%20Networks.pptx)
+- [Caso de estudio: Relación de Agentes](./Caso%20de%20Prueba%2001/Social%20Networks.pptx)
 - [Tratamiento de datos](./Caso%20de%20Prueba%2001/Social%20Network.ipynb)
 - [Reporte Power BI](./Caso%20de%20Prueba%2001/Tarea%2001%20-%20Social%20Network.pbix)
+
+### Semana 02: Principal Component Analysis:
+- [Caso de estudio: Lavado de dinero](./Caso%20de%20Prueba%2002/PrincipalComponentAnalysis.pptx)
+- [Tratamiento de datos](./Caso%20de%20Prueba%2002/Compartir/Tarea%2002.ipynb)
+- [Reporte Power BI](./Caso%20de%20Prueba%2002/Compartir/Lavado%20de%20dinero.pbix)
+
+### Semana 03: Churn Analysis
+- [Caso de estudio: Retiro temprano](./Caso%20de%20Prueba%2003/Churn_Analysis.pptx)
+- [Tratamiento de datos](./Caso%20de%20Prueba%2003/Tarea%2003.ipynb)
+- [Reporte Power BI](./Caso%20de%20Prueba%2003/Retiro%20temprano.pbix)
+
+### Semana 04: Time Series
+- [Caso de estudio: Análisis de ventas de cigarros](./Caso%20de%20Prueba%2004/TimeSeries.pptx)
+- [Tratamiento de datos](./Caso%20de%20Prueba%2004/Tarea%2004.ipynb)
+- [Reporte Power BI](./Caso%20de%20Prueba%2004/Análisis%20de%20Ventas%20Cigarros%20Marlboro.pbix)
+
+### Semana 05: Hierarchival Time Series Forecasting
+- [Caso de estudio: Cartera de crédito](./Caso%20de%20Prueba%2005/HierarchicalTimeSeries.pptx)
+- [Tratamiento de datos](./Caso%20de%20Prueba%2005/Compartir/Caso%20de%20Prueba%2005.ipynb)
+- [Reporte Power BI](./Caso%20de%20Prueba%2005/Compartir/Series%20de%20Tiempo%20jerárquicos.pbix)
+
+### Semana 06: Modelos lineales mixtos
+- [Caso de estudio: Elasticidad precio demanda](./Caso%20de%20Prueba%2006/Modelos%20lineales%20mixtos.pptx)
+- [Tratamiento de datos](./Caso%20de%20Prueba%2006/Elasticidad%20Precio%20-%20Demanda%20Aguacates.pbix)
+- [Reporte Power BI](./Caso%20de%20Prueba%2006/Elasticidad%20Precio%20-%20Demanda%20Aguacates.pbix)
+
+### Proyecto Final: Histórico de Ventas y predicción de la demanda
+- [Tratamiento de datos](./Proyecto%20Final/Compartir/Proyecto%20Final.ipynb)
+- [Reporte Power BI](./Proyecto%20Final/Compartir/Histórico%20de%20Ventas.pbix)
+
+### ¿Qué es el Bussines Intelligence en la Industria?
+- [Video](https://youtu.be/PB3kr2hw3nA)
